f4090588f31c680b217641744f3f38d62ff8172f
Dharshini2901 <dharshinidr05@gmail.com>
2026-03-23 14:16:17 +0530

    Auto create superuser on Render

--- build.sh
+++ build.sh
@@ -2,3 +2,12 @@
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
+python manage.py shell -c "
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'dharshinidr05@gmail.com', 'admin1234', role='mentor')
+    print('Superuser created')
+else:
+    print('Superuser already exists')
+"
